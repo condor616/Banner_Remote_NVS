bf5ea2e65253045d0496db723af76f7d4e1fc378
Condor <condor616@gmail.com>
2013-10-11 17:23:01 +0200

    JSON File read correctly
    Signed-off-by: Condor <condor616@gmail.com>

---
+++ js/jsonReader.js
@@ -0,0 +1,38 @@
+(function($){
+	
+	//Variables
+	//var feed = 'data.json';
+	
+	//Methods
+	var methods = {
+		
+		loadData: function(feed){	
+			$.ajax({
+				type: 'GET',
+				url: feed,
+				crossDomain: true,
+				cache: false,
+				dataType: 'json',
+				success: function(data){
+					alert("JSON Loaded successfully");
+					methods.render(data);
+				},
+				error: function(xhr, ajaxOptions, thrownError){	
+					alert(xhr.status);
+					alert(thrownError);
+				
+				}
+			});
+		},
+		
+		render: function(data){
+		
+		}	
+	};//close Methods object
+	
+	$.fn.jsonReader = function(feed){
+		methods.loadData(feed);
+	}
+	
+	
+})( jQuery );
