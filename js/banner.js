58d53e26979163bd6b5941cbcbf925c47bcea4d2
Condor <condor616@gmail.com>
2013-10-11 12:05:12 +0200

    Added introTitle and IntroDesc effect
    More is coming….
    
    Signed-off-by: Condor <condor616@gmail.com>

--- js/banner.js
+++ js/banner.js
@@ -2,6 +2,8 @@
 $(document).ready(
 	function(){
 		
+		
+		
 		//remove all active class
 		var removeAllActiveClasses = function(){
 			$('div#subpages-nav div.buttons div').each(
@@ -12,42 +14,40 @@ $(document).ready(
 				}
   			);
 		}
-		
-		
-		//the first button becomes active onLoad, and Title & Description get set (for the first button)
-		$firstImg = $('div#subpages-nav div.buttons div').first();
-		$firstImg.addClass('active');
-		$('div.intros span.introTitle').html("Title 1");
-		$('div.intros span.introDesc').html("Desc 1");
-		
-		
-		//hide all the title and text
-		$('div#subpages-nav div.buttons div span.title').css('display','none');
-		$('div#subpages-nav div.buttons div span.desc').css('display','none');
-		
+			
 		
 		//Applying hover event to buttons
 		$('div#subpages-nav div.buttons div').hover(
-		
 			function($e){
 				removeAllActiveClasses();
-				
 				//select the "title" and "desc" tag you want to apply
 				var title = $(this).find('span.title').text();
 				var desc = $(this).find('span.desc').text();
-				
+				var introTitle = $(this).find('span.subpages-title').text();
+				var introDesc = $(this).find('span.subpages-description').text();
 				//set active
 				$(this).addClass('active');	
 				//apply title and desc to the right place
 				$('div.intros span.introTitle').html(title);
 				$('div.intros span.introDesc').html(desc);
 				
+				$('div#subpages div.metadata span#subpages-title').html(introTitle);
+				$('div#subpages div.metadata span#subpages-description').html(introDesc);
+				
 			},
 			function($e){
 				
 			}
-		
 		);
+		
+		/*
+		Once the page is loaded, the first button get automatically hevered
+		*/
+		$firstImg = $('div#subpages-nav div.buttons div').first().addClass('active').trigger('mouseover');
+		
+		
+		
+		
 		
 		
 		
