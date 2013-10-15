5c97f65b919b29433a4a8c141f7c8d6373ba6e4d
Condor <condor616@gmail.com>
2013-10-15 11:05:05 +0200

    Fixed issue with _jsonData
    Now I can read the json file and retrieve data
    
    Signed-off-by: Condor <condor616@gmail.com>

--- js/jquery.jsonReader.js
+++ js/jquery.jsonReader.js
@@ -1,38 +1,39 @@
-// --------------------- Global Variables -------------------------------
-	
-	//for test purpose
-	var _numberOfEntries;
-
-	var _jsonData; //it contains the JSON object
-
-	/*
-	The _scenes array contains objects as elements. 
-	Each object contains the associated "entry".
-	Once the entire JSON file get loaded, we call the method createScenes() that will go through the JSON file extracting all the single scenes assigning 
-	every scene to an object within the array.
-	*/
-	var _jsonScenes = new Array(); 
-	
-//-------------------------END Global Variables ---------------------------------------
 
 
 (function($){
 	
+	
+	// --------------------- Global Variables -------------------------------
+	var pointer;
+	var _jsonData; //it contains the JSON object
+	
+
+
+	
+	//-------------------------END Global Variables ---------------------------------------
+	
+	
+	
 	// --------------- Methods -------------------------------------
 	var methods = {
-		
+
 		//init
 		init: function(settings){
+			
+			pointer = this;
 			
 			_defaultSettings = {
 				feed: "/js/data.json"
 			}
-			
 			//overwrite the default values with the variables
 			if (settings) $.extend(_defaultSettings, settings);	
 			
 			methods.loadData(_defaultSettings.feed);
+			
 		},
+		
+		
+		
 		
 		
 		//loaddata
@@ -44,9 +45,9 @@
 				cache: false,
 				dataType: 'json',
 				success: function(data){
-					methods.render(data);
-				}
-				,
+					_jsonData = data;
+					methods.draw(1);
+				},
 				error: function(xhr, ajaxOptions, thrownError){	
 					alert(xhr.status);
 					alert(thrownError);
@@ -56,46 +57,56 @@
 		},
 		
 		
+		
+		
 		//render
 		render: function(data){
-			_jsonData = data;
 			_numberOfEntries = _jsonData.Entries.length;
 			alert('There are ' + _numberOfEntries + ' entries' );
-			methods.createScenes();
+			//methods.createScenes();
 			
 			alert("JSON Loaded successfully");
 		},
 		
+		
+		
+		
+		
+		/*
 		createScenes: function(){
-			
 			//cycling through the main JSON file and recognizing each single entry
 			for (var i=0; i<_jsonData.Entries.length; i++){
 				_jsonScenes.push(_jsonData.Entries[i]);
 			}
 		},
+		*/
+		
+		
+		
+		
 		
 		/*
 		The following method is responsible to draw a scene. The number of the scene is passed as parameter.
 		*/
 		draw: function(sceneNumber){
 			
-			var _localJson = _jsonScenes[sceneNumber];
+			/*
+			We have to read data from the _jsonData object (the position is determined by sceneNumber)
+			*/
+			
 			
 			// Draw the html page here
-			
-			
-			
+			$(pointer).append($('<img width=\"971\" height=\"347\" src=\"' +_jsonData.Entries[sceneNumber-1].Image + '\">'));	
 		}
 			
 	};
 	// ------------------- END Methods -------------------------------
 	
 	
-	
-		
-	
 	$.fn.jsonReader = function(method){
-		
+		if (methods[method]){
+			return methods[method].apply(this, Array.prototype.slice.call(arguments,1));
+		}
 		if (typeof method == "object" || ! method){
 			return methods.init.apply(this,arguments);
 		}else{
