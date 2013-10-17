42ab80c80110c545658477e3a241d3d8cf8ab90c
Condor <condor616@gmail.com>
2013-10-17 17:30:16 +0200

    Working on Navigation Arrows
    Signed-off-by: Condor <condor616@gmail.com>

--- js/jquery.jsonReader.js
+++ js/jquery.jsonReader.js
@@ -17,14 +17,20 @@
 			pointer = this;
 			
 			_defaultSettings = {
-				feed: "/js/data.json",
-				template: "/templates/template1.html"
+				feed: "/js/data1.json",
+				template: "/templates/main-template.html"
 			}
 			//overwrite the default values with the variables
 			if (settings) $.extend(_defaultSettings, settings);	
 			
 			//methods.loadTemplate(_defaultSettings.template);
+			/*
 			$(pointer).load(_defaultSettings.template, function(){
+				methods.loadData(_defaultSettings.feed)
+			});
+			*/
+			
+			methods.loadTemplate(pointer, _defaultSettings.template, function(){
 				methods.loadData(_defaultSettings.feed)
 			});
 		},
@@ -56,14 +62,19 @@
 			});
 		},
 		
+		
+		
+		loadTemplate: function(pointer, urlTemplate, callback){
+			$(pointer).load(urlTemplate, callback);
+		},
+		
 
 
 		/*
 		The following method is responsible to draw a scene. The number of the scene is passed as parameter.
 		*/
 		draw: function(sceneNumber){
-			
-			
+
 			//remove the class="ACTIVE" from all the buttons
 			var removeAllActiveClasses = function(){
 				$('div#subpages div.buttons div').each(
@@ -74,16 +85,10 @@
 					}
 				);
 			}
-			
-			
-			//We have to read data from the _jsonData object (the position is determined by sceneNumber)
-			
+
 			//IMAGE
 			$('div#nvs_banner img').attr('src',_jsonData.Entries[sceneNumber-1].Image);
-			
-			//DIVs
-			//$('<div class=\"content\"></div>').insertAfter('div#nvs_banner img').append('<div class=\"metadata\"></div>');				
-			
+	
 			//TITLE
 			$('div.metadata').find('h2').html(_jsonData.Entries[sceneNumber-1].Title.Text).css('color', _jsonData.Entries[sceneNumber-1].Title.Color, 'font-size', _jsonData.Entries[sceneNumber-1].Title.Size +'px');
 		
@@ -91,12 +96,6 @@
 			//DESCRIPTION
 			$('div.metadata').find('h3').html(_jsonData.Entries[sceneNumber-1].Description.Text).css('color', _jsonData.Entries[sceneNumber-1].Description.Color, 'font-size', _jsonData.Entries[sceneNumber-1].Description.Size+'px');
 		
-			
-			//Let's create the link-list 
-			//How many links?
-			//console.log("There are " + _jsonData.Entries[sceneNumber-1].Links.length + " links");
-			
-			
 			
 			for (i=0; i<_jsonData.Entries[sceneNumber-1].Links.length; i++){
 				
@@ -108,21 +107,12 @@
 										  '</a>');
 				
 			}
-			
-			
-			//$('<div id=\"subpages-wrapper\"></div>').insertAfter('div.metadata').append('<div id=\"subpages\"></div>');
-			//$('div#subpages').append('<div class=\"metadata\"></div>');
-			
-			
+
 			//At firt page load, introTitle and introDescription are set to the value contained in the first subpage.
 			$('div#subpages div.metadata').find('span#subpages-title').html(_jsonData.Entries[sceneNumber-1].Subpages[0].introTitle);
 			$('div#subpages div.metadata').find('span#subpages-description').html(_jsonData.Entries[sceneNumber-1].Subpages[0].introTitle);
 												  
-			
-			//$('<div id=\"subpages-nav\"></div>').insertAfter('div#subpages div.metadata');
-			//$('div#subpages-nav').append('<div class=\"buttons\"></div>');
-			
-			
+		
 			//Cycle through the button list and draw
 			
 			for (var j=0; j<_jsonData.Entries[sceneNumber-1].Subpages.length; j++){
@@ -135,7 +125,12 @@
 				$('div.icon-'+_jsonData.Entries[sceneNumber-1].Subpages[j].iconType+':eq(j)').append('<span class=\"subpages-description hidden\"></span>');
 			}
 			
-			
+
+			//apply click event to buttons
+			$('div#subpages div.buttons div').click(function(e) {
+                alert("You clicked on the button");
+				
+            });
 		
 			
 			//apply hover event to buttons
@@ -153,12 +148,8 @@
 					$('div#subpages div.metadata span#subpages-title').html(_jsonData.Entries[sceneNumber-1].Subpages[index].introTitle);
 					$('div#subpages div.metadata span#subpages-description').html(_jsonData.Entries[sceneNumber-1].Subpages[index].introDescription);
 				}
-			
 			);
 			$('div#subpages-nav div.buttons div').first().addClass('active').trigger('mouseover');
-
-			
-	
 		}
 			
 	};
